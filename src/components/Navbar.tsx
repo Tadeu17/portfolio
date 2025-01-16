c027682d7ed604e3a76adb5556b0042e5a31ad5c
Tadeu17 <17.tadeu.1989@gmail.com>
2025-01-16 18:40:18 +0000

    working responsive landing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,46 +37,59 @@ const Navbar = () => {
       {/* Main Navbar */}
       <nav className="fixed top-0 left-0 right-0 z-40 bg-black/90 backdrop-blur-sm">
         <div className="container mx-auto px-6">
-          <div className="flex items-center justify-between h-20">
-            <span className="text-2xl font-bold text-white">Tadeu</span>
+          <div className="flex flex-wrap items-center justify-between min-h-20">
+            <span className="md:w-auto h-20 flex items-center text-2xl font-bold text-white">Tadeu</span>
 
-            {/* Desktop Menu */}
-            <div className="hidden md:flex gap-8">
-              {['landing', 'about', 'resume', 'skills', 'projects', 'contact'].map((item) => (
-                <a
-                  key={item}
-                  href={`#${item}`}
-                  className="relative inline-block px-1 py-2 group"
-                >
-                  <span
-                    className={`text-sm font-medium ${activeSection === item ? 'text-yellow-500' : 'text-white'
-                      }`}
-                  >
-                    {item.toUpperCase()}
-                  </span>
-                  <span
-                    className={`absolute left-0 bottom-0 h-0.5 bg-yellow-500 
-                      transition-all duration-1000 ease-in-out
-                      ${activeSection === item ? 'w-full' : 'w-0 group-hover:w-full'}`}
-                    style={{
-                      left: '50%',
-                      transform: 'translateX(-50%)'
-                    }}
-                  />
-                </a>
-              ))}
+            <div className='md:hidden h-20 flex-grow'></div>
+
+            {/* Menu Container */}
+            <div
+              className={`absolute top-20 left-0 right-0 bg-black/90 backdrop-blur-sm
+    transform transition-all duration-500 overflow-hidden
+    ${isMenuOpen ? 'max-h-[100vh] ease-in' : 'max-h-0 ease-out'}`}
+            >
+              {/* Menu Items */}
+              <div className="container mx-auto px-6 py-4">
+                <div className="flex flex-col gap-y-4">
+                  {['landing', 'about', 'resume', 'skills', 'projects', 'contact'].map((item) => (
+                    <a
+                      key={item}
+                      href={`#${item}`}
+                      className="relative inline-block px-1 py-2 group w-fit"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <span
+                        className={`text-sm font-medium 
+                          ${activeSection === item ? 'text-yellow-500' : 'text-white'}`
+                        }
+                      >
+                        {item.toUpperCase()}
+                      </span>
+                      <span
+                        className={`absolute left-0 bottom-0 h-0.5 bg-yellow-500 transition-all duration-1000 ease-in-out
+                                  ${activeSection === item ? 'w-full' : 'w-0 group-hover:w-full'}`
+                        }
+                        style={{
+                          left: '50%',
+                          transform: 'translateX(-50%)'
+                        }}
+                      />
+                    </a>
+                  ))}
+                </div>
+              </div>
             </div>
 
+
             {/* Mobile Menu Button */}
-            <div className="md:hidden flex items-center gap-4">
+            <div className="md:w-auto md:hidden flex items-center gap-4">
               <button
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
-                className="flex gap-1 items-center text-gray-400 hover:text-white"
+                className="h-20 flex gap-1 items-center text-gray-400 hover:text-white"
               >
                 <Bars3Icon className="h-6 w-6 text-white" />
                 MENU
               </button>
-              <span className="text-white">Menu is {isMenuOpen.toString()}</span>
             </div>
           </div>
         </div>
