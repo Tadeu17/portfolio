cf7302beb667fcf7eb28739e542f67ba96b26990
Tadeu17 <17.tadeu.1989@gmail.com>
2025-01-20 02:38:42 +0000

    added testimonials

---
+++ src/components/sections/Testimonials.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import { useState, useRef } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+
+import { testimonials } from '@/data'
+
+import SectionTitle from '../SectionTitle'
+
+const Testimonials = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const touchStartX = useRef<number | null>(null);
+  const touchEndX = useRef<number | null>(null);
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? testimonials.length - 1 : prevIndex - 1
+    );
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === testimonials.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handleTouchStart = (e: React.TouchEvent<HTMLElement>) => {
+    touchStartX.current = e.touches[0].clientX;
+  };
+
+
+  const handleTouchMove = (e: React.TouchEvent<HTMLElement>) => {
+    touchEndX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchEnd = () => {
+    if (!touchStartX.current || !touchEndX.current) return;
+
+    const deltaX = touchStartX.current - touchEndX.current;
+
+    // Swipe threshold to determine direction
+    if (deltaX > 50) {
+      nextSlide(); // Swipe left
+    } else if (deltaX < -50) {
+      prevSlide(); // Swipe right
+    }
+
+    // Reset touch references
+    touchStartX.current = null;
+    touchEndX.current = null;
+  };
+
+  return (
+    <section
+      id="testimonials"
+      className="container mx-auto px-6 py-20 overflow-hidden"
+    >
+      <SectionTitle title="Testimonials" />
+
+      <div
+        className="relative flex items-center justify-center"
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
+        {/* Carousel Wrapper */}
+        <div
+          className="flex w-full lg:w-3/4 transition-transform duration-500"
+          style={{
+            transform: `translateX(-${currentIndex * 100}%)`,
+          }}
+        >
+          {testimonials.map((testimonial, index) => (
+            <div
+              key={index}
+              className="flex-shrink-0 w-full px-4"
+              style={{ flexBasis: "100%" }}
+            >
+              <div className="bg-gray-800 border-2 border-amber-400 p-8 rounded-lg">
+                <p className="text-white text-lg italic mb-4">
+                  &quot;{testimonial.quote}&quot;
+                </p>
+                <h3 className="text-yellow-400 font-semibold text-xl mb-2">
+                  {testimonial.name}
+                </h3>
+                <p className="text-gray-400 mb-4">{testimonial.title}</p>
+                <a
+                  href={testimonial.source}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-amber-400 underline"
+                >
+                  View Source
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {/* Navigation Buttons */}
+        <button
+          onClick={prevSlide}
+          className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-yellow-400 p-2 rounded-full text-gray-800 hover:bg-yellow-500 focus:outline-none"
+        >
+          <ChevronLeftIcon className="w-6 h-6" />
+        </button>
+        <button
+          onClick={nextSlide}
+          className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-yellow-400 p-2 rounded-full text-gray-800 hover:bg-yellow-500 focus:outline-none"
+        >
+          <ChevronRightIcon className="w-6 h-6" />
+        </button>
+      </div>
+
+      {/* Dots Navigation */}
+      <div className="flex justify-center mt-6">
+        {testimonials.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => setCurrentIndex(index)}
+            className={`w-3 h-3 mx-1 rounded-full transition-colors duration-300 focus:outline-none ${index === currentIndex ? "bg-yellow-400" : "bg-gray-500"
+              }`}
+          ></button>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
---
+++ src/data.ts
@@ -0,0 +1,39 @@
+export const testimonials = [
+  {
+    "quote": "I had the pleasure of working with Tadeu at Mammoth, where he brought incredible energy and focus to the team. His unwavering commitment to refining our frontend practices transformed the way we operate. Tadeu made significant contributions in selecting the optimal technology stack, aligning component architecture, and ensuring clarity in backend demands. Additionally, his ability to collaborate with design and product owners and maintain high discipline in ticket management was commendable. Beyond his technical skills, Tadeu is a caring leader who genuinely invested time in guiding and mentoring everyone he led. He was always willing to share his expertise, offer support, and foster a collaborative atmosphere. The team\'s progress over the past three years speaks volumes to his technical leadership and dedication",
+    "name": "Pankaj Lal",
+    "title": "Data Management Innovator at Mammoth Analytics with Deep SaaS expertise",
+    "source": "https://www.linkedin.com/in/tadeumarques/",
+  },
+  {
+    "quote": "I had the pleasure of working closely with Tadeu as my front-end manager, where he consistently demonstrated great management skills and a personable demeanor. Throughout our time working together, Tadeu exhibited a good ability to oversee and guide the front-end development projects with confidence and clarity. Tadeu\'s leadership style is marked by his human approach and affable personality, which made collaborating with him really enjoyable. He fostered an environment of open communication and mutual respect within our team, allowing for the free exchange of ideas and constructive feedback. One of Tadeu's standout qualities is his unwavering confidence in the abilities of his team members. He consistently provided encouragement and support. His optimism was infectious and served as a driving force behind our team\'s progress. Beyond his managerial skills, Tadeu's genuine concern for the well-being and professional growth of his team members was evident in his actions. He took the time to understand our individual strengths and aspirations, offering guidance tailored to each team member\'s needs. In summary, Tadeu is a effective leader who possesses the combination of managerial acumen, interpersonal skills, and genuine empathy. His confidence in his team, coupled with his optimistic outlook, creates an environment where individuals thrive and excel. Should you have any further inquiries regarding Tadeu\'s qualifications or contributions, please do not hesitate to reach out to me. I am more than happy to provide additional insight into his leadership capabilities.",
+    "name": "Filipe Silva",
+    "title": "Frontend developer at Mammoth Analytics",
+    "source": "https://www.linkedin.com/in/tadeumarques/",
+  },
+  {
+    "quote": "I have worked with Tadeu on frontend development. He has great skills. Working with him I learned a lot of things. He has strong work ethics. He is cordial and motivating. I always benifited talking to him on personal and techinical things. He has a good and positive vibe.",
+    "name": "Pedro Braz",
+    "title": "Co-founder at HelpMeChoose",
+    "source": "https://www.linkedin.com/in/tadeumarques/",
+  },
+  {
+    "quote": "His problem-solving skills are unmatched, making him a key asset to our company.",
+    "name": "Yogeshwar Trehan",
+    "title": "Senior Software Engineer at Mammoth Analytics",
+    "source": "https://www.linkedin.com/in/tadeumarques/",
+  },
+  {
+    "quote": "Having worked with Tadeu was a pleasure. We had an excellent partnership when jointly developing a large solution, Tadeu proved to be an excellent VueJs developer, mastering the technology with wisdom and experience.",
+    "name": "Ronaldo Quionha de Jesus",
+    "title": "Full Stack developer at Tips4y",
+    "source": "https://www.linkedin.com/in/tadeumarques/",
+  },
+  {
+    "quote":
+      "Tadeu was a great front-end leader. He has a lot of experience mainly in design patterns and great readable code. We work together using Vuejs in which he has a lot of experience and mastery, especially with pure Javascript, which he has extensive knowledge. Always willing to help arrive at the best solution for any problem that may arise. He was a great mentor in terms of the front-end, he helped my professional development and growth! I would like to work with him again! I wish the best always!",
+    "name": "Thiago Lago",
+    "title": "Junior developer at Tips4y",
+    "source": "https://www.linkedin.com/in/tadeumarques/",
+  },
+];
