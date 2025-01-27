b04ef458cd659c1e224c09c2e589a34371ae143e
Tadeu17 <17.tadeu.1989@gmail.com>
2025-01-27 14:08:36 +0000

    meta keyword typo fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "@/components/Navbar";
 export const metadata: Metadata = {
   title: "Tadeu Marques Portfolio",
   description: "Portfolio of Frontend developer and manager Tadeu Marques",
-  keywords: ['frontend', 'develper', 'engineering manager', 'engineer', 'software development']
+  keywords: ['frontend', 'developer', 'engineering manager', 'engineer', 'software development']
 };
 
 export default function RootLayout({
