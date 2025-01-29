e22ec1894dac5ca2ba75c611902bcdbfea0e148a
Tadeu17 <17.tadeu.1989@gmail.com>
2025-01-29 11:45:57 +0000

    added version to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 
 import type { Metadata } from "next";
 import "./globals.css";
+import packageJson from '../../package.json';
 import Navbar from "@/components/Navbar";
+
+const version = packageJson.version;
 
 export const metadata: Metadata = {
   title: "Tadeu Marques Portfolio",
   description: "Portfolio of Frontend developer and manager Tadeu Marques",
-  keywords: ['frontend', 'developer', 'engineering manager', 'engineer', 'software development']
+  keywords: ['frontend', 'developer', 'engineering manager', 'engineer', 'software development'],
+  other: {
+    version
+  }
 };
 
 export default function RootLayout({
